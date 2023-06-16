dcb54d075ed46fab3c1a82d1678b2ba5757d126c
laasyanayudu <81281050+laasyanayudu@users.noreply.github.com>
2023-06-15 20:38:38 -0400

    Update setup_control.sh

--- setup_control.sh
+++ setup_control.sh
@@ -2,3 +2,12 @@
 #generate passwordless SSH
 runuser -u cn991538 -- ssh-keygen -q -t rsa -f /users/cn991538/.ssh/id_rsa -N
 runuser -u cn991538 -- cat /users/cn991538/.ssh/id_rsa.pub >> /users/cn991538/.ssh/authorized_keys
+
+#setup nfs for key sharing
+apt update
+apt install -y nfs-kernel-server
+mkdir var/nfs/keys -p
+cp /users/cn991538/.ssh/id_rsa* /var/nfs/keys
+chown nobody:nogroup /var/nfs/keys
+echo "var/nfs/keys"
+systemctl restart 
