5cba27442c711fdb8bd53ce3d199c86bf894fb01
laasyanayudu <81281050+laasyanayudu@users.noreply.github.com>
2023-06-15 21:40:40 -0400

    Update setup_control.sh

--- setup_control.sh
+++ setup_control.sh
@@ -11,3 +11,18 @@ cp /users/cn991538/.ssh/id_rsa* /var/nfs/keys
 chown nobody:nogroup /var/nfs/keys
 echo "/var/nfs/keys 192.168.1.2(rw,sync,no_root_squash,no_subtree_check)" | sudo tee -a /etc/exports
 systemctl restart nfs-kernel-server
+
+# setup ansible
+apt-add-repository -y ppa:ansible/ansible
+apt update
+apt install -y ansible
+cp /local/repository/hosts /etc/ansible/
+
+# setup lamp
+git clone https://github.com/do-community/ansible-playbooks.git
+cd ansible-playbooks/lamp_ubuntu1804/
+cp /local/repository/default.yml vars/
+HOST=$(hostname -f)
+sed -i "s/HOSTNAME/$HOST/g" vars/default.yml
+ansible-playbook playbook.yml -l server1 -u cn991538
+
