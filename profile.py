96406468f4aad2af1cfbc9b1b245d759b498bf64
laasyanayudu <81281050+laasyanayudu@users.noreply.github.com>
2023-06-22 18:42:37 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -8,12 +8,14 @@ prefixForIP = "192.168.1."
 link = request.LAN("lan")
 
 # Create a XenVM
-for i in range(2):
+for i in range(3):
   if i == 0:
-    node = request.XenVM("control")
-  else: 
-    node = request.XenVM("host")
-    
+    node = request.XenVM("webserver")
+  elif i == 1: 
+    node = request.XenVM("observer")
+  else:
+    node = request.XenVM("ldap")       
+
   node.routable_control_ip = "true"
   node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU20-64-STD"
   iface = node.addInterface("if" + str(i))
@@ -21,10 +23,8 @@ for i in range(2):
   iface.addAddress(rspec.IPv4Address(prefixForIP + str(i + 1), "255.255.255.0"))
   link.addInterface(iface)
   
-  if i == 0:
-    node.addService(rspec.Execute(shell="sh", command="sudo bash /local/repository/setup_control.sh"))
-  else
-    node.addService(rspec.Execute(shell="sh", command="sudo bash /local/repository/setup_host.sh"))     
+  #if i == 0:
+  #  node.addService(rspec.Execute(shell="sh", command="sudo bash /local/repository/setup_apache.sh"))
     
 # Print the RSpec to the enclosing page.
 portal.context.printRequestRSpec()
